ac310a041dc40d81eb200eb78ea680bde51f1c64
infouniverseforloop <info.universefor.loop@gmail.com>
2025-11-12 20:32:23 +0600

    manipulationDetector.js

---
+++ intelligence/manipulationDetector.js
@@ -0,0 +1,6 @@
+// intelligence/manipulationDetector.js
+function detectManipulation(candles) {
+  // placeholder heuristic, returns false by default
+  return false;
+}
+module.exports = { detectManipulation };
